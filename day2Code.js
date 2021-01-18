48064da812dec8f3ec310310a2317c2b0ad93799
Blake <blakebonasera@gmail.com>
2021-01-18 13:01:25 -0800

    Bubble sort in Javascript

--- day2Code.js
+++ day2Code.js
@@ -30,3 +30,29 @@ function isPalindrome2(string){
     return true;
 }
 console.log(isPalindrome2(string))
+
+//Bubble Sort
+// O(n^2)
+const array =[66,255,25,6,1];
+function bubbleSort(array){
+    let isSorted = false;
+    let counter = 0;
+    while(!isSorted){
+        isSorted=true;
+        for( let i=0; i < array.length-1-counter; i++){
+            if(array[i]>array[i+1]){
+                swap(i, i+1, array);
+                isSorted=false;
+            }
+        }
+        counter++;
+    }
+    return array;
+}
+
+function swap(i, j, array){
+    const temp = array[j];
+    array[j]=array[i];
+    array[i]=temp;
+}
+console.log(bubbleSort(array));
