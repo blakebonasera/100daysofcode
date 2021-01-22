4275ff8e14cfac6d17865569b9d38be67b52bd35
Blake <blakebonasera@gmail.com>
2021-01-22 10:43:56 -0800

    Add Favorites and start remove

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,51 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import MovieList from './components/MovieList';
-import MovieListHeading from './components/MovieListHeading';
-import SearchBox from './components/SearchBox';
-import AddFavorite from './components/AddToFavorites';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavorite from "./components/AddToFavorites";
 
 const App = () => {
-	const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
   const [favorites, setFavorites] = useState([]);
 
-	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
-		const response = await fetch(url);
-		const responseJson = await response.json();
+    const response = await fetch(url);
+    const responseJson = await response.json();
 
-		if (responseJson.Search) {
-			setMovies(responseJson.Search);
-		}
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
   };
-  
+
   const addFavoriteMovie = (movie) => {
     const newFavoriteList = [...favorites, movie];
-    setFavorites(newFavoriteList)
-  }
+    setFavorites(newFavoriteList);
+  };
 
-	useEffect(() => {
-		getMovieRequest(searchValue);
-	}, [searchValue]);
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
-	return (
-		<div className='container-fluid movie-app'>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Movies' />
-				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-			</div>
-			<div className='row'>
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">
         <MovieList
           movies={movies}
           favoriteComponent={AddFavorite}
-          hdaleFavoritesClick={addFavoriteMovie} 
+          handleFavoritesClick={addFavoriteMovie}
         />
-			</div>
-		</div>
-	);
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favorites" />
+      </div>
+      <div className="row">
+        <MovieList movies={favorites} favoriteComponent={AddFavorite} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
