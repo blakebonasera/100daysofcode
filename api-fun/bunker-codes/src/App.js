cfdc9fd25d5cc01053615b4f2fbcb53d30310bbf
Blake <blakebonasera@gmail.com>
2021-01-26 12:19:40 -0800

    Warzone Codes

--- api-fun/bunker-codes/src/App.js
+++ api-fun/bunker-codes/src/App.js
@@ -3,7 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    
+    <div className="container">
+      <div className='title'>
+        <h1 className='text-danger text-center bg-dark'>Warzone Codes</h1>
+      </div>
+      <div className='body'>
+      <ul class="list-group ">
+  <li class="list-group-item bg-dark text-danger"><strong>Park</strong> - 60274513</li>
+  <li class="list-group-item bg-dark text-danger"><strong>TV Station</strong> - 27495810</li>
+  <li class="list-group-item bg-dark text-danger"> <strong>North Junkyard</strong> - 87624851</li>
+  <li class="list-group-item bg-dark text-danger"><strong>South Junkyard</strong> - 97264138</li>
+  <li class="list-group-item bg-dark text-danger"> <strong>Warzone Farmland code</strong> - 49285163</li>
+  <li class="list-group-item bg-dark text-danger"> <strong>Warzone Prison shack code</strong> - 72948531</li>
+</ul>
+      </div>
+    </div>
   );
 }
 
