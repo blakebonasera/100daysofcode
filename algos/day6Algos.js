9cce20a4a348cabd91a03828371ed708a5d6b265
Blake <blakebonasera@gmail.com>
2021-01-27 12:42:59 -0800

    Longest Balancedd Substring O(n)

--- algos/day6Algos.js
+++ algos/day6Algos.js
@@ -1,6 +1,6 @@
 // LongestBalancedSubstring O(n^3) | time O(n) space
 
-fucntion longestBalancedSubstring(string){
+function longestBalancedSubstring(string){
     let maxLength = 0;
     for (let i = 0; i < string.length; i++){
         for (let j = i + 2; j < string.length + 1; j+=2){
@@ -25,4 +25,27 @@ function isBalanced(string) {
         }
     }
     return openParensStack.length === 0;
+}
+
+// O(n) Time | O(n) Space
+
+function longestBalancedSubstring(string){
+    let maxLength = 0, idxStack = [];
+    idxStack.push(-1);
+
+    for (let i = 0; i < string.length; i++){
+        if (string[i] === "("){
+            idxStack.push(i);
+        } else {
+            idxStack.pop();
+            if (idxStack.length === 0){
+                idxStack.push(i);
+            } else {
+                const balancedSubstringStartIdx = idxStack[idxStack.length - 1];
+                const currentLength = i - balancedSubstringStartIdx;
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+    }
+    return maxLength;
 }
