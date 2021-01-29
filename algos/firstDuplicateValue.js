224c1385f71730de065268ef3e3aa0b6142fcec7
Blake <blakebonasera@gmail.com>
2021-01-29 10:13:28 -0800

    First Duplicate Value O(n) time O(n) space

--- algos/firstDuplicateValue.js
+++ algos/firstDuplicateValue.js
@@ -16,3 +16,13 @@ function firstDuplicateValue(array) {
     return array[minimumSecondIndex];
 }
 
+// O(n) Time | O(n) Space
+
+function firstDuplicateValue(array) {
+    const seen = new Set();
+    for (const value of array) {
+        if (seen.has(value)) return value;
+        seen.add(value);
+    }
+    return -1;
+}
