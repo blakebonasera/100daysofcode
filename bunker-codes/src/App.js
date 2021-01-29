c0f4cc28b459113482423b909677fb888761a27b
Blake <blakebonasera@gmail.com>
2021-01-29 11:09:56 -0800

    Bunker codes v2
    v3 I will have an accordian that you click which code you want and it will show it highlighted on a map with its code on it as well as an actual image of where it is in game

--- bunker-codes/src/App.js
+++ bunker-codes/src/App.js
@@ -1,4 +1,4 @@
-
+import map from './bunker-map.jpg'
 import './App.css';
 
 function App() {
@@ -7,13 +7,17 @@ function App() {
     <div className="container bg-dark">
       <h1 className='text-danger text-center bg-dark'>Warzone Codes</h1>
       <ul class="list-group border-danger ">
-        <li class="list-group-item bg-dark text-danger border-danger"><strong>Park</strong> - 60274513</li>
-        <li class="list-group-item bg-dark text-danger border-danger"><strong>TV Station</strong> - 27495810</li>
-        <li class="list-group-item bg-dark text-danger border-danger"> <strong>North Boneyard</strong> - 87624851</li>
-        <li class="list-group-item bg-dark text-danger border-danger "><strong>Boneyard</strong> - 97264138</li>
+      <li class="list-group-item bg-dark text-danger border-danger"> <strong>1 DAM</strong> - RED ACCESS CARD</li>
+        <li class="list-group-item bg-dark text-danger border-danger"> <strong>3 North Boneyard</strong> - 87624851</li>
+        <li class="list-group-item bg-dark text-danger border-danger "><strong>4 Boneyard</strong> - 97264138</li>
+        <li class="list-group-item bg-dark text-danger border-danger"> <strong>5 South </strong> - RED ACCESS CARD</li>
+        <li class="list-group-item bg-dark text-danger border-danger"><strong>6 Park</strong> - 60274513</li>
+        <li class="list-group-item bg-dark text-danger border-danger"> <strong>7 Prison shack</strong> - 72948531</li>
+        <li class="list-group-item bg-dark text-danger border-danger"> <strong>10 Quarry</strong> - RED ACCESS CARD</li>
+        <li class="list-group-item bg-dark text-danger border-danger"><strong> TV Station</strong> - 27495810</li>
         <li class="list-group-item bg-dark text-danger border-danger"> <strong>Farmland</strong> - 49285163</li>
-        <li class="list-group-item bg-dark text-danger border-danger"> <strong>Prison shack</strong> - 72948531</li>
       </ul>
+      <img src={map} alt="map"/>
     </div>
       </>
   );
